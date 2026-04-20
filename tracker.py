4024fc8cd6bee1a0978dec2177c85f6cd74e1e53
Fluxxion88 <manchulyantsev.e@gmail.com>
2026-04-19 23:40:45 -0700

    json update

--- tracker.py
+++ tracker.py
@@ -1,1 +1,17 @@
-print('Hello World')
+import json
+
+
+data = {
+    "sections": [
+        {
+            "name": "NeetCode",
+            "tasks": [
+                {"name": "twosum", "time": 0, "Status": "not started"},
+                {"name": "valid anagram", "time": 0, "Status": "not started"}
+            ]
+        }
+    ]
+}
+with open("profile.json", "w", encoding="utf-8") as f:
+    json.dump(data, f, indent=4, ensure_ascii=False)
+
