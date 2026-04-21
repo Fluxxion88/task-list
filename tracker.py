744f9c687458288a1f2400b3c86fe7b26930cf18
Fluxxion88 <manchulyantsev.e@gmail.com>
2026-04-21 16:03:23 -0700

    Add list command

--- tracker.py
+++ tracker.py
@@ -1,17 +1,33 @@
 import json
+import argparse
+
+with open("profile.json", 'r', encoding='utf-8') as file:
+    data = json.load(file)
+
+def add_class(som):
+    data["sections"].append({"name": som, "tasks": []})
+    
+    with open("profile.json", "w", encoding='utf-8') as file:
+        json.dump(data, file, ensure_ascii=False, indent=4)
+        print("Новый раздел добавлен!! ")
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("action")
+parser.add_argument("value", nargs='?', default=None)
+
+args = parser.parse_args()
+
+if args.action == "add_class":
+    add_class(args.value)
+elif args.action == "list":
+    x = 1
+    for i in data["sections"]:
+        print(f"{x}. {i["name"]}")
+        x += 1
 
 
-data = {
-    "sections": [
-        {
-            "name": "NeetCode",
-            "tasks": [
-                {"name": "twosum", "time": 0, "Status": "not started"},
-                {"name": "valid anagram", "time": 0, "Status": "not started"}
-            ]
-        }
-    ]
-}
-with open("profile.json", "w", encoding="utf-8") as f:
-    json.dump(data, f, indent=4, ensure_ascii=False)
+
+else:
+    print("Ты чё упоротый?! ты же сам писал этот код, как блять ошибся олух")
 
